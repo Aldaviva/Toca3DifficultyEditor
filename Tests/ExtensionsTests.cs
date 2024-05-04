87062d1ee49d9ae194c69a505028214f9dfb3d8b
Ben Hutchison <ben@aldaviva.com>
2024-05-03 17:07:52 -0700

    Added test for string to byte serialization extension method

---
+++ Tests/ExtensionsTests.cs
@@ -0,0 +1,16 @@
+﻿using Toca3DifficultyEditor;
+
+namespace Tests;
+
+public class ExtensionsTests {
+
+    [Theory]
+    [InlineData("B", new byte[] { 0x42 })]
+    [InlineData("ÿ", new byte[] { 0xC3, 0xBF })]
+    [InlineData("\u2603", new byte[] { 0xE2, 0x98, 0x83 })]
+    [InlineData("\ud83d\udca9", new byte[] { 0xF0, 0x9F, 0x92, 0xA9 })]
+    public void stringToBytes(string input, byte[] expected) {
+        input.ToBytes().Should().Equal(expected);
+    }
+
+}
